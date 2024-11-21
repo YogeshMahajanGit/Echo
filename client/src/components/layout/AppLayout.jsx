a904be2dfed6acea13dc49781e0a64a7ba6b271c
Your_Name <Your_Email>
2024-11-21 15:16:27 +0530

    modify app layout

--- client/src/components/layout/AppLayout.jsx
+++ client/src/components/layout/AppLayout.jsx
@@ -1,56 +1,75 @@
+/* eslint-disable react/display-name */
+import React from "react";
+import { useParams } from "react-router-dom";
 import Title from "../shared/Title";
 import ChatList from "../specific/ChatList";
 import Header from "./Header";
 import { Grid } from "@mui/material";
 import { chatSample } from "../constants/sampleData";
 import Profile from "../specific/Profile";
-// import { useParams } from "react-router-dom";
-// const params = useParams();
 
-/* eslint-disable react/display-name */
-const AppLayout = () => (WrappedComponent) => {
-  return (props) => {
-    // const chatId = params.chatId;
+const AppLayout = () => {
+  return (WrappedComponent) => {
+    return (props) => {
+      const InnerComponent = () => {
+        const params = useParams();
+        const chatId = params.chatId;
 
-    function handleDeleteChat(e, _id, groupChat) {}
+        const handleDeleteChat = React.useCallback((e, _id, groupChat) => {
+          // delete logic
+          console.log("Deleting chat:", _id, groupChat);
+        }, []);
 
-    return (
-      <>
-        <Title />
-        <Header />
-        <Grid container sx={{ height: "calc(100vh - 4rem)" }}>
-          <Grid
-            height={"100%"}
-            item
-            sm={4}
-            md={3}
-            sx={{
-              display: { xs: "none", sm: "block" },
-            }}
-          >
-            <ChatList
-              chats={chatSample}
-              // chatId={chatId}
-              handleDeleteChat={handleDeleteChat}
-            />
-          </Grid>
-          <Grid xs={12} sm={8} md={5} lg={6} height={"100%"} bgcolor={"purple"}>
-            <WrappedComponent {...props} />
-          </Grid>
-          <Grid
-            md={4}
-            lg={3}
-            height={"100%"}
-            sx={{
-              display: { xs: "none", md: "block" },
-              padding: "2rem",
-            }}
-          >
-            <Profile />
-          </Grid>
-        </Grid>
-      </>
-    );
+        return (
+          <>
+            <Title />
+            <Header />
+            <Grid container sx={{ height: "calc(100vh - 4rem)" }}>
+              <Grid
+                height={"100%"}
+                item
+                sm={4}
+                md={3}
+                sx={{
+                  display: { xs: "none", sm: "block" },
+                }}
+              >
+                <ChatList
+                  chats={chatSample}
+                  chatId={chatId}
+                  handleDeleteChat={handleDeleteChat}
+                />
+              </Grid>
+              <Grid
+                item
+                xs={12}
+                sm={8}
+                md={5}
+                lg={6}
+                height={"100%"}
+                bgcolor={"purple"}
+              >
+                <WrappedComponent {...props} />
+              </Grid>
+              <Grid
+                item
+                md={4}
+                lg={3}
+                height={"100%"}
+                sx={{
+                  display: { xs: "none", md: "block" },
+                  padding: "2rem",
+                }}
+              >
+                <Profile />
+              </Grid>
+            </Grid>
+          </>
+        );
+      };
+
+      return <InnerComponent />;
+    };
   };
 };
 
