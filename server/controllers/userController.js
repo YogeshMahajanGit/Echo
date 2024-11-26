acae3730864e178d78d33fe1d0e278abfc99bd6f
Your_Name <Your_Email>
2024-11-26 11:20:44 +0530

    add all mongoose models

---
+++ server/controllers/userController.js
@@ -0,0 +1,5 @@
+function login(req, res) {
+  res.send("Hello word login");
+}
+
+export { login };
--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 import express from "express";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 
+app.use("/api/user", userRouter);
+
+//testing
 app.get("/", (req, res) => {
-  res.send("Welcome");
+  res.send("Welcome Home");
 });
 
 app.listen(3000, () => {
